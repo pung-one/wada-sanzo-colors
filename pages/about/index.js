05a79f2b0217d4c105eaece0dd6030f63dac9d90
pung-one <p-ungerer@systemli.org>
2023-03-17 14:37:19 +0100

    change symbols in about-page text to avoid deploy error and move handleUpdateFavs from toggle function to useEffect hook

--- pages/about/index.js
+++ pages/about/index.js
@@ -41,13 +41,13 @@ export default function AboutPage() {
           color was not just a decorative element, but a powerful tool for
           communication and expression. Wada developed his own unique color
           system, which was based on the traditional Japanese sense of color, as
-          well as Western color theory. Wada's color system focused on the
+          well as Western color theory. Wada&apos;s color system focused on the
           relationships between colors and their emotional and cultural
           associations. He categorized colors into different schemes, such as
           monochromatic, complementary, triadic, and more. He also explored the
           use of color in different artistic mediums, such as painting,
-          printmaking, and textile design. Wada's work on color theory had a
-          significant impact on the development of modern graphic design in
+          printmaking, and textile design. Wada&apos;s work on color theory had
+          a significant impact on the development of modern graphic design in
           Japan. His teachings and philosophy influenced generations of
           designers and artists, and his ideas about color continue to be
           studied and applied in various fields today.
@@ -56,8 +56,8 @@ export default function AboutPage() {
       <Article>
         <Headline>A Dictionary of Color Combinations Vol. 1</Headline>
         <p>
-          "A Dictionary of Color Combinations Vol. 1" is a collection of 348
-          color combinations based on 159 colors. This website offers an
+          &quot;A Dictionary of Color Combinations Vol. 1&quot; is a collection
+          of 348 color combinations based on 159 colors. This website offers an
           interactive adaptation of the collection that allows users to access
           and copy the CMYK, RGB, HEX, and LAB codes (Thanks to{" "}
           <Link href="https://github.com/mattdesl/dictionary-of-colour-combinations">
@@ -67,10 +67,10 @@ export default function AboutPage() {
           of color combinations) by Wada Sanzo, from which the book is derived,
           was published from 1933 to 1934. It was a period permeated with a new
           atmosphere brought on by the influence of western culture. The concept
-          of "combining colors" was not generally recognized at the time, so,
-          samplers of color combinations containing specific color combination
-          patterns were highly unusual. Hashoku Soukan was the precursor of such
-          samplers.
+          of &quot;combining colors&quot; was not generally recognized at the
+          time, so, samplers of color combinations containing specific color
+          combination patterns were highly unusual. Hashoku Soukan was the
+          precursor of such samplers.
         </p>
       </Article>
       <Footer>
