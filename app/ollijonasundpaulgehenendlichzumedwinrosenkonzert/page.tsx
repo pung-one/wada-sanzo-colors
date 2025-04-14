4cc8b54631c7969027f9d9549bf76585c8b0d6a7
pung-one <p-ungerer@systemli.org>
2025-04-14 18:44:29 +0200

    move styled pages to client side

--- app/ollijonasundpaulgehenendlichzumedwinrosenkonzert/page.tsx
+++ app/ollijonasundpaulgehenendlichzumedwinrosenkonzert/page.tsx
@@ -1,39 +1,5 @@
-"use client";
-
-import Countdown from "react-countdown";
-import styled from "styled-components";
+import { OlliJonasPaul } from "@/components/OlliJonasPaul/OlliJonasPaul";
 
 export default function CountdownPage() {
-  return (
-    <>
-      <h1>hhi</h1>
-      {/* <PageContainer>
-        <Headline>
-          Wann gehen Olli, Jonas und Paul endlich zum Edwin Rosen Konzert?
-        </Headline>
-
-        <Countdown date={"2025-11-08T12:20:00.00"} />
-      </PageContainer> */}
-    </>
-  );
+  return <OlliJonasPaul />;
 }
-
-const PageContainer = styled.main`
-  margin: 0 0 2vh;
-  display: flex;
-  flex-direction: column;
-  gap: 50px;
-  span {
-    text-align: center;
-  }
-  @media screen and (min-width: 1024px), screen and (orientation: landscape) {
-    width: 70%;
-    margin-left: 30%;
-  }
-`;
-
-const Headline = styled.h1`
-  margin-top: 150px;
-  padding: 20px;
-  text-align: center;
-`;
