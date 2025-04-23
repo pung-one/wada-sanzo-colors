666e06c1111837e5b4986aec15e41f68ad372ffd
pung-one <p-ungerer@systemli.org>
2025-04-23 20:24:56 +0200

    test

--- pages/ollijonasundpaulgehenendlichzumedwinrosenkonzert/index.js
+++ pages/ollijonasundpaulgehenendlichzumedwinrosenkonzert/index.js
@@ -1,12 +1,33 @@
 import Countdown from "react-countdown";
 import styled from "styled-components";
+import { useEffect, useRef } from "react";
 
 export default function CountdownPage() {
+  const videoRef = useRef(null);
+
+  async function startCamera() {
+    try {
+      const stream = await navigator?.mediaDevices?.getUserMedia({
+        video: true,
+        audio: false,
+      });
+
+      if (videoRef?.current) {
+        videoRef.current.srcObject = stream;
+      }
+    } catch (err) {
+      console.error("Error accessing camera:", err);
+    }
+  }
+
   return (
     <PageContainer>
       <Headline>
         Wann gehen Olli, Jonas und Paul endlich zum Edwin Rosen Konzert?
       </Headline>
+
+      <button onClick={startCamera}>start</button>
+      <video ref={videoRef} autoPlay muted playsInline />
 
       <Countdown date={"2025-11-08T12:20:00.00"} />
     </PageContainer>
