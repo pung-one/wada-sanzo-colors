3a6a91380aba029b7b677647f63cf85f9a8fcfb8
pung-one <p-ungerer@systemli.org>
2025-05-18 05:12:32 +0200

    extend sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,9 @@
+import { colorsWithSlug } from "@/data/colors";
+import { createCombinationArray } from "@/utils/helper";
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+  const urls: MetadataRoute.Sitemap = [
     {
       url: "https://www.wada-sanzo-colors.com",
       lastModified: new Date(),
@@ -20,5 +22,35 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 0.9,
     },
+    ...colorsWithSlug.map((c) => ({
+      url: `https://www.wada-sanzo-colors.com/colors/${c.slug}`,
+      lastModified: new Date(),
+      changeFrequency: "yearly" as
+        | "yearly"
+        | "always"
+        | "hourly"
+        | "daily"
+        | "weekly"
+        | "monthly"
+        | "never"
+        | undefined,
+      priority: 0.5,
+    })),
+    ...createCombinationArray(colorsWithSlug).map((c) => ({
+      url: `https://www.wada-sanzo-colors.com/combinations/${c.id}`,
+      lastModified: new Date(),
+      changeFrequency: "yearly" as
+        | "yearly"
+        | "always"
+        | "hourly"
+        | "daily"
+        | "weekly"
+        | "monthly"
+        | "never"
+        | undefined,
+      priority: 0.5,
+    })),
   ];
+
+  return urls;
 }
